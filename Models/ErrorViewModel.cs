1bc918006f9cc75424a953ccae81aa52a5703286
brian419 <json10@crimson.ua.edu>
2024-10-19 11:26:20 -0500

    Initial commit for Fall2024-Assignment3

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2024_Assignment3_json10.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
