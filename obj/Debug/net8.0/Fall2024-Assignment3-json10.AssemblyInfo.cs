ccf84a320085e3438720a548dc0cc29c2c26aed2
brian419 <json10@crimson.ua.edu>
2024-10-26 08:57:10 -0500

    changing description of manage movies and manage actors on homepage

--- obj/Debug/net8.0/Fall2024-Assignment3-json10.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fall2024-Assignment3-json10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fall2024-Assignment3-json10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c4367a6990f4171e3321ba99f39a7cf293d22b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26edbf04ec709659261d522c94b77d57fbf3edb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fall2024-Assignment3-json10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fall2024-Assignment3-json10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
